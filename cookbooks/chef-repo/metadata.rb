c1ffa6325f183c99c865bffcda8d15243409e1e8
sandeepraonayini <sandeepraonayini9@gmail.com>
2017-07-23 21:19:11 -0500

    modified metatdata.rb

--- cookbooks/chef-repo/metadata.rb
+++ cookbooks/chef-repo/metadata.rb
@@ -6,6 +6,7 @@ description 'Installs/Configures chef-repo'
 long_description 'Installs/Configures chef-repo'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'ubuntu'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
